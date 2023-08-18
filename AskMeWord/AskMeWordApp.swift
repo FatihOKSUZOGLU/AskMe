525a4c66b0547ddee44062018d6ed0a152bdab05
Fatih ÖKSÜZOĞLU <fatihoksuzoglu@hotmail.com>
2023-08-18 23:37:47 +0300

    Initial Commit

---
+++ AskMeWord/AskMeWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskMeWordApp.swift
+//  AskMeWord
+//
+//  Created by Fatih OKSUZOGLU on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AskMeWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
